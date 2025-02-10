b98380575d08026ef13fcb6f2a19391ea02ffe1d
Andrey Lazurko <rasgis@yandex.ru>
2025-02-10 13:23:45 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <i className="fa fa-camera-retro"></i>
+            <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+            <i className="fa fa-user-circle" aria-hidden="true"></i>
+            <a className="fa fa-user" aria-hidden="true"></a>
+        </>
+    );
+}
+
+export default App;
