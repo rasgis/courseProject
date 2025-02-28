47a023822623f457a4968c14dd57c5972d68a81d
Andrey <rasgis@yandex.ru>
2025-02-28 10:51:59 +0300

    add NotFound

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,8 +1,11 @@
-export const Input = ({ className, ...props }) => {
+import { forwardRef } from 'react';
+
+export const Input = forwardRef(({ className, ...props }, ref) => {
 	return (
 		<input
 			className={className}
 			{...props}
+			ref={ref}
 		/>
 	);
-};
+});
